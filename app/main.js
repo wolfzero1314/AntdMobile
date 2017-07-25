8f64597b6320f8241aefb28c17fa4e88535adaab
wolfzero1314 <77227370@163.com>
2017-07-25 15:36:50 +0800

    app first commit

---
+++ app/main.js
@@ -0,0 +1,13 @@
+
+
+import React from "react";
+
+import ReactDOM from "react-dom";
+
+import TabBarExample from "./components/Component.jsx";
+
+
+ReactDOM.render(
+    <TabBarExample/>,
+    document.getElementById("content")
+)
